478e6fdc975a32a5a773ca8abefd15b6bb727788
Daples <daples1@gmail.com>
2025-07-07 10:21:16 +0200

    Adding plot and evaluate methods for boxes

--- utils/box.py
+++ utils/box.py
@@ -1,10 +1,15 @@
 from __future__ import annotations
 from dataclasses import dataclass
-from typing import ClassVar
+from typing import ClassVar, Any
+from matplotlib.axes import Axes
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
 
 
 @dataclass
 class Box:
+    name: str
     xmin: float
     xmax: float
     ymin: float
@@ -49,14 +54,67 @@ class Box:
 
         return self.__getattribute__(f"{dim}max") - self.__getattribute__(f"{dim}min")
 
-    def expand(self, l_xm: float, l_xp: float, l_y: float, l_z: float) -> Box:
+    def expand(
+        self, new_name: str, l_xm: float, l_xp: float, l_y: float, l_z: float
+    ) -> Box:
         """Create a guidelines box based on the current box."""
 
         return Box(
+            name=new_name,
             xmin=self.xmin - l_xm,
             xmax=self.xmax + l_xp,
             ymin=self.ymin - l_y,
             ymax=self.ymax + l_y,
             zmin=self.zmin,
             zmax=self.zmax + l_z,
+        )
+
+    def plot(self, ax: Axes, color: str, **kwargs: Any) -> Axes:
+        """"""
+
+        bbox = patches.Rectangle(
+            (self.xmin, self.ymin),
+            self.get_length("x"),
+            self.get_length("y"),
+            linewidth=1,
+            edgecolor=color,
+            facecolor="none",
+            label=self.name,
+            **kwargs,
+        )
+        ax.add_patch(bbox)
+        return ax
+
+    def evaluate(self, bbox_buildings: Box, L_max: float, flow_dim: int) -> None:
+        """"""
+
+        flow = self.dims[flow_dim]
+        z = self.dims[2]
+
+        # Calculate the blockage ratio
+        area_blockage = bbox_buildings.compute_area(axis=flow_dim)
+        area_domain = self.compute_area(axis=flow_dim)
+        blockage_ratio = area_blockage / area_domain
+
+        # Calculate directional blockage ratio
+        l_buildings = bbox_buildings.get_length(flow)
+        l_domain = self.get_length(flow)
+        blockage_ratio_l = l_buildings / l_domain
+
+        h_buildings = bbox_buildings.get_length(z)
+        h_domain = self.get_length(z)
+        blockage_ratio_h = h_buildings / h_domain
+
+        # Directional blockage ratio (single container at largest blockage, 45°)
+        blockage_ratio_container = L_max / l_domain
+
+        print(
+            f"\nBounding box ({self.name}):\n"
+            + f"x: [{self.xmin:.2f}, {self.xmax:.2f}],\n"
+            + f"y: [{self.ymin:.2f}, {self.ymax:.2f}],\n"
+            + f"z: [{self.zmin:.2f}, {self.zmax:.2f}],\n"
+            + f"Blockage ratio: {blockage_ratio:.2%}\n"
+            + f"Blockage ratio (L{flow}, all buildings): {blockage_ratio_l:.2%}\n"
+            + f"Blockage ratio (H{z}, all buildings): {blockage_ratio_h:.2%}\n"
+            + f"Blockage ratio (Single container): {blockage_ratio_container:.2%}"
         )
